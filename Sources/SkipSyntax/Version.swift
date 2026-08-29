f1f00b6c2439f3c1b194788d1398d87f92357c4d
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-08-29 17:40:51 -0400

    Release 1.9.8

--- Sources/SkipSyntax/Version.swift
+++ Sources/SkipSyntax/Version.swift
@@ -5,4 +5,4 @@
 // NOTE: this variable is updated by the `scripts/release_skip.sh` script when the version is bumped.
 
 /// The current version of Skip.
-public let skipVersion = "1.9.7"
+public let skipVersion = "1.9.8"
